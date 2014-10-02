2afcb759faa975b12a50acffd50c91691fb41d94
dannyITT <danielsmith2004@gmail.com>
2014-10-02 21:52:49 +0100

    TODO

---
+++ data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#: Title : data.sh
+#: Date : 2 October
+#: Author : Danny
+#: Version :1.0
+#: Description : Question 5 Collecting data from command line
+#: Options : 
+
+
+printf "hello %s you were born in %s and your favouraite color is %s\n" $1 $2 $3
+
+# TODO replace positionaal parameters
+# with parameter data read in using the read command
