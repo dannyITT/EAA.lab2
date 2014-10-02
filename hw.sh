f3bcdee5952e8566fe7c8ac6d89b7dc3fb9e217a
dannyITT <danielsmith2004@gmail.com>
2014-10-02 21:32:34 +0100

    Added Conditional parameters

--- hw.sh
+++ hw.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#: Title : 
-#: Date : 
+#: Title : hw.sh
+#: Date : 2 October
 #: Author : 
 #: Version :
-#: Description : 
+#: Description : this is a hello world script using positional parameters
 #: Options : 
 
-printf "%s\n" "Hello, World!" 
+printf "hello %s nice to meet you\n" $1
 
