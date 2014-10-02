6a1549b6b5f465cac4285395a32761a66cf60b8f
dannyITT <danielsmith2004@gmail.com>
2014-10-02 20:52:20 +0100

    Entered the code for counting CPUs

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 # Date : 2 October 20134
 # Author : danny	
 # Version :1.0
-# Description :this ia test script 
+# Description :this is a CPU Count script
 # Options : 
 
+echo The number of CPUs is
+grep processor/proc/cpuinfo | wc -l
